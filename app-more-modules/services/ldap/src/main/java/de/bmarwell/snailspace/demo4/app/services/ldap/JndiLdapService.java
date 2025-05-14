455781ba30cbac5b59abe2497f4dabce4b3c7793
Benjamin Marwell <bmarwell@apache.org>
2025-05-14 11:32:41 +0200

    add List UserGroup method and tests, relax timeout on UserGroup creation

--- app-more-modules/services/ldap/src/main/java/de/bmarwell/snailspace/demo4/app/services/ldap/JndiLdapService.java
+++ app-more-modules/services/ldap/src/main/java/de/bmarwell/snailspace/demo4/app/services/ldap/JndiLdapService.java
@@ -1,9 +1,15 @@
 package de.bmarwell.snailspace.demo4.app.services.ldap;
 
 import de.bmarwell.snailspace.demo4.app.common.value.User;
+import de.bmarwell.snailspace.demo4.app.common.value.UserGroup;
 import de.bmarwell.snailspace.demo4.app.common.value.UserId;
 import de.bmarwell.snailspace.demo4.app.services.api.LdapService;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.random.RandomGenerator;
 
 public class JndiLdapService implements LdapService {
 
@@ -23,5 +29,25 @@ public class JndiLdapService implements LdapService {
         }
 
         return Optional.empty();
+    }
+
+    public List<UserGroup> getUserGroupNames(UserId userId) {
+        try {
+            Thread.sleep(Long.parseLong(System.getProperty("method.timeout", "100")));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        List<UserGroup> groups = new ArrayList<>();
+
+        RandomGenerator randomGenerator = RandomGenerator.getDefault();
+        for (int i = 0; i < 10; i++) {
+            var bb = new byte[24];
+            randomGenerator.nextBytes(bb);
+            String group = Base64.getEncoder().encodeToString(bb);
+            groups.add(new UserGroup(group));
+        }
+
+        return Collections.unmodifiableList(groups);
     }
 }
